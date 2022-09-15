ab70cbd0270aa637930fe1e0ab9a4bcec9bf2df4
Augusto Rogerio <augustoo.rd@gmail.com>
2022-09-15 00:29:47 -0300

    Mudando o bd

--- routes/ucBp.py
+++ routes/ucBp.py
@@ -1,5 +1,5 @@
 #importando o blueprint
-from flask import Blueprint
+from flask import Blueprint, render_template
 from ..extensions import db
 from ..models.uc import Uc
 
@@ -10,4 +10,8 @@ ucBp = Blueprint('ucBp', __name__)
 def uc_list():
 #    return "Teste"
 
-    db.create_all()
+#    db.create_all()
+
+#Adiciona o acesso a banco e a chamada do template
+    ucs_query = Uc.query.all()
+    return render_template('uc_list.html', ucs=ucs_query)
